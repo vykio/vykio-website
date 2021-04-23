1802925f589c3591ba92825ea5357363e3c7880c
Alexandre <alexandrevasseur06@gmail.com>
2021-04-23 12:02:56 +0200

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,10 +1,32 @@
+const Twitch = require("twitch.tv-api");
+const twitch = new Twitch({
+  id: process.env.TWITCH_ID,
+  secret: process.env.TWITCH_SECRET
+})
 
+const userToStream = "vykio";
+
+async function getStream() {
+  let data = await twitch.getUser(userToStream);
+  return data;
+}
 
 export default (req, res) => {
 
-
+  const stream = await getStream();
+  var live = false;
+  
+   if (stream.stream === null) {
+     live = false;
+   } else {
+     if(stream.stream["stream_type"] == 'live') {
+       live = true;
+     } else {
+      live = false;
+     }
+   }
 
   res.statusCode = 200
-  res.json({ name: 'John Doe' })
+  res.json({ live })
 }
 
