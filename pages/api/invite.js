40df2779c66e01d6de7a274f44521c0eefe13c15
Alexandre <alexandrevasseur06@gmail.com>
2021-05-15 19:51:27 +0200

    Update invite.js

--- pages/api/invite.js
+++ pages/api/invite.js
@@ -1,4 +1,4 @@
-export default async (req, res) => {
+module.exports = async (req, res) => {
 
   res.redirect("https://discord.com/api/oauth2/authorize?client_id=832535979868684318&permissions=2148005952&scope=bot");
   
